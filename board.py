e36c246db6d30fc8f7c0e73181a240d731068269
jttomichek42 <jttomichek42@tntech.edu>
2022-02-19 19:47:26 -0600

    Made changes to the mouse track, added play functionality

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 #file deals with all the logic for the board
 import numpy as np
+from variables import *
 
 #function that creates a board
 def create_board(row, col):
@@ -9,3 +10,30 @@ def create_board(row, col):
 #function that plays a piece of value at a certain board
 def play_piece(board, row, col, piece_val):
     board[row][col] = piece_val 
+
+#function that determines if a column is full
+def is_column_full(col, board):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return False
+    
+    return True
+
+#function that determines if the board is full
+def is_board_full(board):
+    for c in range(COL_COUNT):
+        for r in range(ROW_COUNT):
+            if board[r][c] == 0:
+                return False
+    
+    return True
+
+#function that gets the highest avaliable row for a column to drop a piece
+def get_highest_row(col, board):
+    for r in reversed(range(ROW_COUNT)):
+        if board[r][col] == 0:
+            return r
+
+#function that checks for possible win conditions
+def check_for_win(board):
+    pass
--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 #file deals with all the logic for the game and the main
-import numpy as np
 import pygame
 import math
 from board import *
@@ -11,7 +10,7 @@ def create_screen():
     background_color = BLUE
 
     #create the screen and set title
-    screen = pygame.display.set_mode((COL_COUNT*SQUARESIZE, ROW_COUNT*SQUARESIZE))
+    screen = pygame.display.set_mode((COL_COUNT*SQUARESIZE, (ROW_COUNT+1)*SQUARESIZE))
     pygame.display.set_caption("Connect Four game")
 
     #set bg color of screen
@@ -34,8 +33,6 @@ def draw_board(screen, color):
 def draw_board_plays(screen, board):
     for c in range(COL_COUNT):
         for r in range(ROW_COUNT):
-            if board[r][c] == 0:
-                pygame.draw.circle(screen, BLACK, (c*SQUARESIZE+SQUARESIZE/2, r*SQUARESIZE+SQUARESIZE/2),  RADIUS)
             if board[r][c] == 1:
                 pygame.draw.circle(screen, RED, (c*SQUARESIZE+SQUARESIZE/2, r*SQUARESIZE+SQUARESIZE/2),  RADIUS)
             if board[r][c] == 2:
@@ -43,58 +40,93 @@ def draw_board_plays(screen, board):
 
     pygame.display.update()
 
-#function that deals with the logic of tracking the mouse for highlighting the column
-def mouse_track(board, board2, turn_val, mouse_x):
-    if 0 <= mouse_x <= 100:
-        board[0][0] = turn_val
-    elif board2[0][0] != turn_val:
-        board[0][0] = 0
+#function that deals with the logic of tracking the mouse for underlining the column
+def mouse_track(screen, turn_val, column):
+    color = BLACK
+    if turn_val == 1:
+        color = RED
+    if turn_val == 2:
+        color = YELLOW
 
-    if 101 <= mouse_x <= 200:
-        board[0][1] = turn_val
-    elif board2[0][1] != turn_val:
-        board[0][1] = 0
+    if column == 0:
+        pygame.draw.rect(screen, color, (0, 600, SQUARESIZE, SQUARESIZE/20))
+    else:
+        pygame.draw.rect(screen, BLUE, (0, 600, SQUARESIZE, SQUARESIZE/20))
 
-    if 201 <= mouse_x <= 300:
-        board[0][2] = turn_val
-    elif board2[0][2] != turn_val:
-        board[0][2] = 0
+    if column == 1:
+        pygame.draw.rect(screen, color, (100, 600, SQUARESIZE, SQUARESIZE/20))
+    else:
+        pygame.draw.rect(screen, BLUE, (100, 600, SQUARESIZE, SQUARESIZE/20))
 
-    if 301 <= mouse_x <= 400:
-        board[0][3] = turn_val
-    elif board2[0][3] != turn_val:
-        board[0][3] = 0
+    if column == 2:
+        pygame.draw.rect(screen, color, (200, 600, SQUARESIZE, SQUARESIZE/20))
+    else:
+        pygame.draw.rect(screen, BLUE, (200, 600, SQUARESIZE, SQUARESIZE/20))
 
-    if 401 <= mouse_x <= 500:
-        board[0][4] = turn_val
-    elif board2[0][4] != turn_val:
-        board[0][4] = 0
+    if column == 3:
+        pygame.draw.rect(screen, color, (300, 600, SQUARESIZE, SQUARESIZE/20))
+    else:
+        pygame.draw.rect(screen, BLUE, (300, 600, SQUARESIZE, SQUARESIZE/20))
 
-    if 501 <= mouse_x <= 600:
-        board[0][5] = turn_val
-    elif board2[0][5] != turn_val:
-        board[0][5] = 0
+    if column == 4:
+        pygame.draw.rect(screen, color, (400, 600, SQUARESIZE, SQUARESIZE/20))
+    else:
+        pygame.draw.rect(screen, BLUE, (400, 600, SQUARESIZE, SQUARESIZE/20))
 
-    if 601 <= mouse_x <= 700:
-        board[0][6] = turn_val
-    elif board2[0][6] != turn_val:
-        board[0][6] = 0
+    if column == 5:
+        pygame.draw.rect(screen, color, (500, 600, SQUARESIZE, SQUARESIZE/20))
+    else:
+        pygame.draw.rect(screen, BLUE, (500, 600, SQUARESIZE, SQUARESIZE/20))
+
+    if column == 6:
+        pygame.draw.rect(screen, color, (600, 600, SQUARESIZE, SQUARESIZE/20))
+    else:
+        pygame.draw.rect(screen, BLUE, (600, 600, SQUARESIZE, SQUARESIZE/20))
+    
 
 #function that deals with all my realtime events
-def check_for_events(screen, board, board2):
+def check_for_events(screen, board):
     running = True
     turn_val = 1 #determining player 1 or 2
     draw_board(screen, BLACK)
+    game_over = False
+    myfont = pygame.font.SysFont("monospace", 75)
     while(running):
         for event in pygame.event.get():
-
+            
+            #tracks mouse position
             mouse = pygame.mouse.get_pos()
             mouse_x = mouse[0]
+            current_col = math.floor(mouse_x/100)
 
-            if turn_val == 1:
-                mouse_track(board, board2, turn_val, mouse_x)
-            elif turn_val == 2:
-                mouse_track(board, board2, turn_val, mouse_x)
+            #constanly tracking the mouse position and drawing the appropriate underline
+            mouse_track(screen, turn_val, current_col)
+
+            #when the mouse is pressed 
+            if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+                #check if column is fulk
+                if not is_column_full(current_col, board):
+                    #get row to play in
+                    r_pos = get_highest_row(current_col, board) 
+                    #plays a piece
+                    play_piece(board, r_pos, current_col, turn_val)
+
+                    #
+                    # put check for win function here    
+                    #
+
+                    #checks if the board is full if no win condition
+                    #should be elif
+                    if is_board_full(board):
+                        txt = myfont.render("Board Full!", 1, BLACK)
+                        screen.blit(txt, ((ROW_COUNT*SQUARESIZE)/2 - SQUARESIZE*2, (COL_COUNT-1)*SQUARESIZE+10))
+                        game_over = True
+
+                    #switches the turn
+                    if turn_val == 1:
+                        turn_val = 2
+                    elif turn_val == 2:
+                        turn_val = 1
 
             if event.type == pygame.QUIT:
                 running = False
@@ -103,17 +135,14 @@ def check_for_events(screen, board, board2):
 
 #main 
 def main():
-    #board that manages the draw engine
-    draw_board = create_board(ROW_COUNT, COL_COUNT)
-    #this board tracks what pieces were played where
-    play_board = create_board(ROW_COUNT, COL_COUNT)
+    #init the game
+    pygame.init()
+    #making the board
+    board = create_board(ROW_COUNT, COL_COUNT)
+    #creating the screen
     screen = create_screen()
-
-    # board[3][1] = 1
-    # board[5][2] = 2
-
-    check_for_events(screen, draw_board, play_board)
-
+    #run check for events
+    check_for_events(screen, board)
 
 if __name__ == "__main__":
     main()
